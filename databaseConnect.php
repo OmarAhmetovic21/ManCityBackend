0b20e910670f5afb897b440106eb1f6c949d9400
OmarAhmetovic21 <92754632+OmarAhmetovic21@users.noreply.github.com>
2024-05-10 09:10:43 +0200

    Database connection

---
+++ databaseConnect.php
@@ -0,0 +1,17 @@
+<?php
+
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', '');
+define('NAME', 'mancity');
+
+$conn = new mysqli(HOST, USER, PASS, NAME);
+mysqli_set_charset($conn, 'utf8');
+
+if($conn -> connect_error){
+    die('Connection failed' . $conn -> connect_error);
+}
+else 
+   echo 'Connected successfully';
+
+?>
